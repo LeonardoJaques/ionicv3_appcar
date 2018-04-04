af8e1681c0977d4786e8e1f05d961d9b55692e77
Leonardo Jaques <jaques.projetos@outlook.com>
2018-04-04 11:26:30 -0300

    Adicionando acessórios ao veículo selecionado

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { Acessorio } from '../../modelos/acessorio';
 
 
 @IonicPage()
@@ -11,14 +12,27 @@ import { Carro } from '../../modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios: Acessorio[];
+  private _precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     this.carro = this.navParams.get('carroSelecionado');
+    this._precoTotal = this.carro.preco;
+    this.acessorios = [
+      { nome: 'Freio ABS', preco: 800 },
+      { nome: 'Ar-condicionado', preco: 1000 },
+      { nome: 'MP3 player', preco: 500 }
+    ];
+  }
+  atualizaTotal(ativado: boolean, acessorio : Acessorio ){
+    ativado ?
+    this._precoTotal += acessorio.preco:
+    this._precoTotal -= acessorio.preco;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EscolhaPage');
+  get precoTotal(){
+    return this._precoTotal;
   }
-
+  
 }
